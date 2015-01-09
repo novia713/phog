efb204325b7b12e9a298e61ceaeaa8186cd8c94b
leandro <leandro@localhost.localdomain>
2015-01-09 12:08:46 +0100

    beautify

--- src/Novia713/Phog/Phog.php
+++ src/Novia713/Phog/Phog.php
@@ -1,13 +1,14 @@
 <?php
-
 /*
- * This file is part of Phog.
- *
- * (c) 2015 Leandro Vázquez
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of Phog.
+*
+* (c) 2015 Leandro Vázquez
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+
 
 /**
  * Tool for printing PHP variables
@@ -15,64 +16,62 @@
  *
  * @author leandro <leandro@leandro.org>
  */
-
 error_reporting(E_ALL);
 class Phog
-{
 
+{
   var $_print;
-  function __construct() {
-    $this->_print = function($msg, $mode){
+  function __construct()
+  {
+    $this->_print = function ($msg, $mode)
+    {
       echo "<script>console." . $mode . "(" . $msg . ");</script>";
     };
   }
 
-  public function log($txt){
+  public
+
+  function log($txt)
+  {
     $this->printjson($txt, "log");
   }
 
-  public function info($txt){
+  public
+
+  function info($txt)
+  {
     $this->printjson($txt, "info");
   }
 
-  public function warn($txt){
+  public
+
+  function warn($txt)
+  {
     $this->printjson($txt, "warn");
   }
 
-  public function table($txt){
+  public
+
+  function table($txt)
+  {
     $this->printobj($txt, "table");
   }
 
   // log, info & warn → quoted text
-  private function printjson($txt, $mode){
 
-    $txt = json_encode( json_encode($txt) );
+  private
+  function printjson($txt, $mode)
+  {
+    $txt = json_encode(json_encode($txt));
     $this->_print->__invoke($txt, $mode);
   }
 
   // table → unquoted object
-  private function printobj($txt, $mode){
 
+  private
+  function printobj($txt, $mode)
+  {
     $txt = json_encode($txt);
     $this->_print->__invoke($txt, $mode);
   }
-
-
-
 }
-
-
-
-
-
-$languages = [
-     ["name"=> "JavaScript", "fileExtension"=> ".js"] ,
-     ["name"=> "TypeScript", "fileExtension"=> ".ts"] ,
-     ["name"=> "CoffeeScript", "fileExtension"=> ".coffee"]
-];
-
-$phog = new Phog;
-$phog->log(new StdClass);
-$phog->info("lorem ipsum");
-$phog->table($languages);
-$phog->warn([1,"anda",3,4,5]);
